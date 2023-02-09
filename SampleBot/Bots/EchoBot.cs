66f8dd71650f7098c21e9a8b62181208015b1ad8
Atsuki Matsuo <matsuo.de@gmail.com>
2023-02-09 23:51:39 +0900

    adaptive cards

--- SampleBot/Bots/EchoBot.cs
+++ SampleBot/Bots/EchoBot.cs
@@ -8,6 +8,11 @@ using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.Bot.Builder;
 using Microsoft.Bot.Schema;
+using System.IO;
+using System;
+using AdaptiveCards;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 
 namespace EchoBot.Bots
 {
@@ -21,12 +26,59 @@ namespace EchoBot.Bots
 
         protected override async Task OnMembersAddedAsync(IList<ChannelAccount> membersAdded, ITurnContext<IConversationUpdateActivity> turnContext, CancellationToken cancellationToken)
         {
-            var welcomeText = "Hello and welcome!";
+            var heroCard = new HeroCard
+            {
+                Title = "This is a title",
+                Text = "This is a text.",
+            };
+            var imagePath = Path.Combine(Environment.CurrentDirectory, @"img", "sample.jpg");
+            var imageData = Convert.ToBase64String(File.ReadAllBytes(imagePath));
+            heroCard.Images = new List<CardImage>{
+                new CardImage{
+                    Url= $"data:image/png;base64,{imageData}"
+                }
+            };
+
+            var adaptiveCard = new AdaptiveCard(new AdaptiveSchemaVersion(1, 0));
+            adaptiveCard.Body.Add(new AdaptiveTextBlock()
+            {
+                Text = "Hello",
+                Size = AdaptiveTextSize.ExtraLarge
+            });
+
+            adaptiveCard.Body.Add(new AdaptiveChoiceSetInput
+            {
+                Type = AdaptiveChoiceSetInput.TypeName,
+                Id = "MultiSelect",
+                // Value = cardData.MultiSelect,
+                IsMultiSelect = false,
+                Choices = new List<AdaptiveChoice>
+                        {
+                            new AdaptiveChoice() { Title = "True", Value = "true" },
+                            new AdaptiveChoice() { Title = "False", Value = "false" },
+                        },
+            });
+            adaptiveCard.Actions.Add(new AdaptiveSubmitAction
+            {
+                Type = AdaptiveSubmitAction.TypeName,
+                Title = "Submit",
+                Data = new JObject { { "submitLocation", "messagingExtensionFetchTask" } },
+            }
+            );
+
+            var activity = MessageFactory.Attachment(heroCard.ToAttachment());
+
+            activity.Attachments.Add(new Attachment()
+            {
+                ContentType = "application/vnd.microsoft.card.adaptive",
+                Content = JsonConvert.DeserializeObject(adaptiveCard.ToJson()),
+            });
+
             foreach (var member in membersAdded)
             {
                 if (member.Id != turnContext.Activity.Recipient.Id)
                 {
-                    await turnContext.SendActivityAsync(MessageFactory.Text(welcomeText, welcomeText), cancellationToken);
+                    await turnContext.SendActivityAsync(activity, cancellationToken);
                 }
             }
         }
